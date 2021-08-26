8bc6a304adb02e7ab067907498bf7bbd35e7d393
jayce <1261719702@qq.com>
2021-08-26 20:28:24 +0800

    redux

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+     <h1>redux</h1>
+    );
+  }
+}
+
+export default App;
